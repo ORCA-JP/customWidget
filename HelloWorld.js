9e82eb3899e3f0c3e036e3633b12cac3a03c5e88
ORCA-JP <73975245+ORCA-JP@users.noreply.github.com>
2020-11-06 10:22:56 +0900

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,35 +1,38 @@
 (function()  {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-        <h1>Hello World</h1>
-    `;
+    tmpl.innerHTML = ``;
 
     customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
-
 
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    		        this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			this._firstConnection = false;
+			this._tagContainer;
+			this._tagType = "h1";
+			this._tagText = "Hello World";
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
+		this._firstConnection = true;
+		this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
         disconnectedCallback(){
-        
         }
 
          //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
+	onCustomWidgetBeforeUpdate(oChangedProperties) {
+	}
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-            this.redraw();
+	onCustomWidgetAfterUpdate(oChangedProperties) {
+		if (this._firstConnection){
+			this.redraw();
+		}
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
@@ -44,6 +47,16 @@
         }
         */
 
-        redraw(){}
+        redraw(){
+		if(this._tagContainer){
+			this._tagContainer.parentNode.removeChild(this._tagContainer);
+		}
+		
+		var shadow = window.getSelection(this.shadowRoot);
+		this._tagContainer = document.createElement(this._tagType);
+		var theText = document.createTextNode(this._tagText);
+		this._tagContainer.appendChild(theText);
+		this._shadowRoot.appendChild(this._tagContainer);
+	}
     });
 })();
