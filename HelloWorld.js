debb941ff6253c04181b05f1fc9d0d23e6124a91
ORCA-JP <73975245+ORCA-JP@users.noreply.github.com>
2021-08-26 16:22:47 +0900

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -52,17 +52,18 @@
 
         //Getters and Setters
         get widgetText() {
-            return this._tagType;
+		console.log("this is get : " + this._tagType);
+		return this._tagText;
         }
         set widgetText(value) {
-            this._tagText = value;
+		this._tagText = value;
+		console.log("this is set : " + value);
         }
         get headingType() {
-            return this._tagType;
+		return this._tagType;
 	}
-
         set headingType(value) {
-            this._tagType = value;
+		this._tagType = value;
         }
         // End - Getters and Setters
 
