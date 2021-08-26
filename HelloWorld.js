b699e5c6eba4273a523da98d972c15e278c96372
ORCA-JP <73975245+ORCA-JP@users.noreply.github.com>
2021-08-26 15:01:02 +0900

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,7 +1,6 @@
 (function()  {
     let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-    `;
+    tmpl.innerHTML = ``;
 
     customElements.define('com-sap-sample-helloworld', class HelloWorld extends HTMLElement {
 
@@ -9,17 +8,23 @@
 		constructor() {
 			super(); 
 			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-            this._tagContainer;
-            this._tagType = "h1";
-            this._tagText = "Hello World";
+      this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+      this._firstConnection = false;
+      this._tagContainer;
+      this._tagType = "h1";
+      this._tagText = "Hello World";
+
+      //Adding event handler for click events
+			this.addEventListener("click", event => {
+			  var event = new Event("onClick");
+				this.dispatchEvent(event);
+      });
 		}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
             this._firstConnection = true;
-            this.redraw();           
+            this.redraw(); 
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -53,6 +58,26 @@
         }
         */
 
+        //Getters and Setters
+        get widgetText() {
+            return this._tagType;
+        }
+
+        set widgetText(value) {
+            this._tagText = value;
+        }
+
+
+        get headingType() {
+            return this._tagType;
+            }
+
+        set headingType(value) {
+            this._tagType = value;
+        }
+
+        // End - Getters and Setters
+
         redraw(){
             if (this._tagContainer){
                 this._tagContainer.parentNode.removeChild(this._tagContainer);
@@ -63,6 +88,8 @@
             var theText = document.createTextNode(this._tagText);    
             this._tagContainer.appendChild(theText); 
             this._shadowRoot.appendChild(this._tagContainer);
+          
+       
 
         }
     
